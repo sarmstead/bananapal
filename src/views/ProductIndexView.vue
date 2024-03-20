0109d4011e610f89af6633232a9c0f45d134334d
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2024-03-19 20:14:08 -0400

    Add products section UI

---
+++ src/views/ProductIndexView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import products from '../../api/products.json'
+console.log(products)
+</script>
+
+<template>
+  <main>
+    <section class="container-fluid products">
+      <div class="row justify-content-center gap-3">
+        <article
+          v-for="product in products"
+          :key="product.id"
+          class="col-sm-6 col-md-3 border border-blue p-2"
+        >
+          <div class="mw-100 d-flex justify-content-center overflow-hidden">
+            <img
+              :src="`${product.image}`"
+              :alt="`Image of ${product.name}`"
+              class="object-fit-contain"
+              height="150px"
+            />
+          </div>
+
+          <h2 class="fw-bold fs-6 mb-1">{{ product.name }}</h2>
+          <p class="mb-3">{{ `$${product.price} / ${product.unit}` }}</p>
+
+          <button class="btn btn-yellow">Add to Basket</button>
+        </article>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+.products {
+  max-width: 952px;
+}
+
+@media (min-width: 576px) {
+  .products article {
+    max-width: 225px;
+  }
+}
+</style>
